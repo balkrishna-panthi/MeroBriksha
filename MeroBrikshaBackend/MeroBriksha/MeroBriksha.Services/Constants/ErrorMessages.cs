8240b3411f20514e9404876ad0e9d5617cc440cd
Balkrishna Panthi <bpi25003@student.mdu.se>
2026-06-19 12:11:25 +0200

    added error messages

---
+++ MeroBrikshaBackend/MeroBriksha/MeroBriksha.Services/Constants/ErrorMessages.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeroBriksha.Services.Constants
+{
+    public  class ErrorMessages
+    {
+        public const string DonationNotFound = "Donation not found.";
+        public const string CampaignNotFound = "Campaign not found.";
+        public static string CampaignNotFoundById(string id) => $"Campaign with ID {id} not found.";
+
+        public const string DonationMustGreaterThanZero = "Donation amount must be greater than zero.";
+
+
+
+
+        public const string DonationAlreadyVerified = "Donation is already verified.";
+        public const string DonationRejectedCannotVerify = "Rejected donation cannot be verified.";
+
+    }
+}
