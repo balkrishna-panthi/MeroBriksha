3c35046bde459c5b213ef86cd7bf261c61537636
Balkrishna Panthi <bpi25003@student.mdu.se>
2026-06-19 12:11:51 +0200

    added conflict exception

---
+++ MeroBrikshaBackend/MeroBriksha/MeroBriksha.Services/Services/Exceptions/ConflictException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeroBriksha.Services.Services.Exceptions
+{
+    public class ConflictException : Exception
+    {
+        public ConflictException(string message) : base(message) { }
+    }
+}
