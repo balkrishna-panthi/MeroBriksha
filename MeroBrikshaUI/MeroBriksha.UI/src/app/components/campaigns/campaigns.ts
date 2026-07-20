06950ba28a0cf11523935411abde533399d26825
Balkrishna Panthi <bpi25003@student.mdu.se>
2026-07-20 11:57:13 +0200

    added NewCampaign button

--- MeroBrikshaUI/MeroBriksha.UI/src/app/components/campaigns/campaigns.ts
+++ MeroBrikshaUI/MeroBriksha.UI/src/app/components/campaigns/campaigns.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { CampaignTable } from '../../widgets/campaign-table/campaign-table'; 
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-campaigns',
-  imports: [CampaignTable],
+  imports: [CampaignTable, MatIconModule, MatButtonModule],
   templateUrl: './campaigns.html',
   styleUrl: './campaigns.css',
 })
