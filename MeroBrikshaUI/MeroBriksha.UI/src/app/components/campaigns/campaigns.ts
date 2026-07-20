c5abceda5e4e5a009897ede19592084a011fdd3a
Balkrishna Panthi <bpi25003@student.mdu.se>
2026-07-20 12:29:17 +0200

    added basic click event in NewCampaign Button

--- MeroBrikshaUI/MeroBriksha.UI/src/app/components/campaigns/campaigns.ts
+++ MeroBrikshaUI/MeroBriksha.UI/src/app/components/campaigns/campaigns.ts
@@ -3,10 +3,19 @@ import { CampaignTable } from '../../widgets/campaign-table/campaign-table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 
+
 @Component({
   selector: 'app-campaigns',
-  imports: [CampaignTable, MatIconModule, MatButtonModule],
+  imports: [CampaignTable, MatIconModule, MatButtonModule, Dialog, MatFormFieldModule, FormsModule],
   templateUrl: './campaigns.html',
   styleUrl: './campaigns.css',
 })
-export class Campaigns {}
+export class Campaigns {
+  onNewCampaignClick() {
+    this.newCampaign();
+  }
+
+  newCampaign() {
+    console.log('New Campaign button clicked');
+  }
+}
