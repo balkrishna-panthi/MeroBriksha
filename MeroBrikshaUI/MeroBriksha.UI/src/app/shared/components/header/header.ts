154a6ccf80fc0793bb10d82f0c963c7b01d799ff
Balkrishna Panthi <bpi25003@student.mdu.se>
2026-08-23 17:11:26 +0200

    implemented toggle for meu in mobile view

--- MeroBrikshaUI/MeroBriksha.UI/src/app/shared/components/header/header.ts
+++ MeroBrikshaUI/MeroBriksha.UI/src/app/shared/components/header/header.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+  isMobileMenuOpen = false;
+
+  toggleMobileMenu(): void {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  closeMobileMenu(): void {
+    this.isMobileMenuOpen = false;
+  }
+}
